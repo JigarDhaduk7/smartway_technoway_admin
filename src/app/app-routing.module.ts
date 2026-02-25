98ebe72382c1270791f3c64f9bff4d8ec8d2d6f8
Ansh Talaviya <anshtalaviya22@gmail.com>
2026-02-25 21:30:19 +0530

    add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './_pages/login/login.component';
 import { AfterloginComponent } from './afterlogin/afterlogin.component';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   {
@@ -12,6 +13,7 @@ const routes: Routes = [
   {
     path: '',
     component: AfterloginComponent,
+    canActivate: [AuthGuard],
     children:[
       { 
         path: 'html', 
