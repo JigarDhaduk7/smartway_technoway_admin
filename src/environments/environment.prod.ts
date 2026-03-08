cee2f6eab0b08946cf43e02c55105fefda8ddae7
Jigar Dhaduk <jigardhduk7@gmail.com>
2026-03-08 15:21:29 +0530

    INIT

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  // apiUrl: 'https://smartwaytechnoway.com/api'
-  apiUrl: 'http://localhost:5000/api'
+  apiUrl: 'https://smartwaytechnoway.com/api'
+  // apiUrl: 'http://localhost:5000/api'
 };
